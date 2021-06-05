ed23fed92d83fe291a42330a3c47e15d7038b125
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-06-06 00:43:01 +0100

    feat: complete redesign of login form
    Created `AuthForm` component to reuse `Login` component functionalities.

---
+++ frontend/src/components/AuthForm.vue
@@ -0,0 +1,104 @@
+<!-- eslint-disable vue/v-slot-style -->
+<template>
+  <q-card class="container row flex-center">
+    <div class="col-md-6 col-sm-8 col-xs-12 col-lg-4 col-xl-3">
+      <q-card class="login-card text-white q-pa-lg q-mx-sm-md q-mx-xs-sm">
+        <q-card-section>
+          <p class="form-logo">Akpoho Invoicing Software</p>
+          <span class="form-title q-mb-md q-pb-md">
+            <slot name="formTitle"></slot>
+          </span>
+          <slot
+            name="formSection"
+            v-bind="{ isSmallScreen: isSmallScreen }"
+          ></slot>
+
+          <slot
+            name="submitSection"
+            v-bind="{ isSmallScreen: isSmallScreen }"
+          ></slot>
+
+          <div class="text-center q-py-lg text-white form-footer">
+            <slot name="formFooter"></slot>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-card>
+</template>
+
+<!-- eslint-disable @typescript-eslint/no-unsafe-return -->
+<!-- eslint-disable @typescript-eslint/no-unsafe-member-access -->
+<!-- eslint-disable @typescript-eslint/restrict-template-expressions -->
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useQuasar } from 'quasar';
+//const strongPassword = helpers.regex('strongPassword', //)
+
+export default defineComponent({
+  name: 'AuthForm',
+  components: {},
+  setup() {
+    const $q = useQuasar();
+
+    const isSmallScreen = computed(() => {
+      return $q.screen.lt.sm;
+    });
+
+    return {
+      isSmallScreen,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.form-logo {
+  font-size: 2rem;
+  color: $purple-10;
+  display: flex;
+  text-align: center;
+  display: block;
+  text-transform: uppercase;
+  margin: 10px auto;
+}
+
+.form-title {
+  font-size: 1.2rem;
+  color: $grey-10;
+  line-height: 1.2;
+  text-align: center;
+  display: block;
+}
+
+.login-card {
+  background-color: $grey-1;
+}
+
+.q-card.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: $purple-2;
+}
+</style>
+
+<style lang="sass" scoped>
+@media (max-width: $breakpoint-xs-max)
+  .form-logo
+    font-size: 1.4rem
+  .form-title
+    font-size: 1rem
+</style>
+
+<style lang="scss">
+.form-footer {
+  color: $grey-10;
+  a {
+    text-decoration: none;
+    &:hover {
+      border-bottom: 1px dotted $purple-10;
+      padding-bottom: 2px;
+    }
+  }
+}
+</style>
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,102 +1,88 @@
 <!-- eslint-disable vue/v-slot-style -->
 <template>
-  <div class="row q-mx-auto justify-center items-center container">
-    <div
-      class="
-        q-gutter-y-md
-        column
-        q-mt-xl-xl q-mt-sm
-        col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-3
-      "
-    >
-      <q-card class="login-card text-white q-pa-lg">
-        <q-card-section>
-          <form class="form validate-form" @submit.prevent="handleLogin">
-            <span class="form-logo">
-              <p class="text-center">Akpoho Invoicing Software</p>
-              <!-- <q-img
-                :src="'https://placeimg.com/1000/800/nature'"
-                spinner-color="white"
-                style="height: 100px; max-width: 150px"
-              /> -->
-            </span>
-            <span class="form-title q-mb-md q-pb-md"> Log In </span>
-            <q-input
-              v-model="form.email"
-              dark
-              bottom-slots
-              clearable
-              type="email"
-              :error="form$.email.$invalid"
-              label="Email"
-              color="white"
-              label-color="white"
-              bg-color="transparent"
-              autofocus
-            >
-              <template v-slot:hint> </template>
-              <template v-slot:error>
-                {{
-                  form$.email.$silentErrors
-                    .map((error) => error.$message)
-                    .join(', ')
-                }}
-              </template>
-            </q-input>
-            <q-input
-              v-model="form.password"
-              dark
-              bottom-slots
-              counter
-              clearable
-              :error="form$.password.$invalid"
-              label="Password"
-              :type="showPassword ? 'text' : 'password'"
-              color="white"
-              label-color="white"
-              bg-color="transparent"
-              class="q-mt-md"
-            >
-              <template v-slot:hint> </template>
-              <template v-slot:error>
-                {{
-                  form$.password.$silentErrors
-                    .map((error) => error.$message)
-                    .join(', ')
-                }}
-              </template>
-              <template v-slot:append>
-                <q-icon
-                  v-if="form.password"
-                  :name="showPassword ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="showPassword = !showPassword"
-                />
-              </template>
-            </q-input>
+  <auth-form>
+    <template #formTitle> Log into your account </template>
 
-            <div class="q-gutter-sm flex justify-center q-mt-md">
-              <q-btn
-                :loading="submitting"
-                color="accent"
-                icon-right="send"
-                label="Submit"
-                @click.prevent="handleLogin"
-              >
-                <template v-slot:loading> Logging In... </template>
-              </q-btn>
-            </div>
+    <template #formSection="{ isSmallScreen }">
+      <form class="form validate-form" @submit.prevent="handleLogin">
+        <q-input
+          v-model="form.email"
+          :dense="isSmallScreen"
+          filled
+          bottom-slots
+          clearable
+          type="email"
+          :error="form$.email.$invalid"
+          label="Email"
+          color="purple-10"
+          label-color="purple-10"
+          bg-color="purple-1"
+          autofocus
+        >
+          <template v-slot:hint> </template>
+          <template v-slot:error>
+            {{
+              form$.email.$silentErrors
+                .map((error) => error.$message)
+                .join(', ')
+            }}
+          </template>
+        </q-input>
+        <q-input
+          v-model="form.password"
+          :dense="isSmallScreen"
+          filled
+          bottom-slots
+          counter
+          clearable
+          :error="form$.password.$invalid"
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          color="purple-10"
+          label-color="purple-10"
+          bg-color="purple-1"
+          class="q-mt-md"
+        >
+          <template v-slot:hint> </template>
+          <template v-slot:error>
+            {{
+              form$.password.$silentErrors
+                .map((error) => error.$message)
+                .join(', ')
+            }}
+          </template>
+          <template v-slot:append>
+            <q-icon
+              v-if="form.password"
+              :name="showPassword ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="showPassword = !showPassword"
+            />
+          </template>
+        </q-input>
+      </form>
+    </template>
 
-            <div class="text-center q-py-lg text-white form-footer">
-              <router-link class="text-white" to="#"
-                >Forgot Password?
-              </router-link>
-            </div>
-          </form>
-        </q-card-section>
-      </q-card>
-    </div>
-  </div>
+    <template #submitSection="{ isSmallScreen }">
+      <q-btn
+        :loading="submitting"
+        :class="{
+          'full-width': isSmallScreen,
+          'text-center flex align-center q-mt-lg q-mx-auto q-px-xl': true,
+        }"
+        color="accent"
+        icon-right="send"
+        label="Submit"
+        @click.prevent="handleLogin"
+      >
+        <template v-slot:loading> Logging In... </template>
+      </q-btn>
+    </template>
+
+    <template #formFooter>
+      <router-link to="#">Forgot Password? </router-link>
+    </template>
+  </auth-form>
 </template>
 
 <!-- eslint-disable @typescript-eslint/no-unsafe-return -->
@@ -115,11 +101,12 @@ import {
 import { useStore } from 'vuex';
 import { Notify } from 'quasar';
 import { useRouter } from 'vue-router';
+import AuthForm from '../components/AuthForm.vue';
 //const strongPassword = helpers.regex('strongPassword', //)
 
 export default defineComponent({
   name: 'Login',
-  components: {},
+  components: { AuthForm },
   setup() {
     const submitting = ref(false);
     const store = useStore();
@@ -201,7 +188,6 @@ export default defineComponent({
 
     return {
       ph: ref(''),
-      dense: ref(false),
       dismissed: ref(false),
       submitting,
       form,
@@ -213,73 +199,4 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped>
-.container {
-  width: 100%;
-  min-height: 100vh;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background: url('/cosmic-blue.jpg');
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  position: relative;
-  z-index: 1;
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background-color: rgba(255, 255, 255, 0.5);
-  }
-}
-
-.form-logo {
-  font-size: 30px;
-  color: white;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-transform: uppercase;
-  margin: 0 auto;
-}
-
-.form-title {
-  font-size: 20px;
-  color: #fff;
-  line-height: 1.2;
-  text-align: center;
-  display: block;
-}
-
-.login-card {
-  background: -webkit-linear-gradient(to top, #7579ff 10%, #b224ef 60%);
-  background: -o-linear-gradient(to top, #7579ff 10%, #b224ef 60%);
-  background: -moz-linear-gradient(to top, #7579ff 10%, #b224ef 60%);
-  background: linear-gradient(to top, #7579ff 10%, #b224ef 60%);
-}
-
-.form-footer a {
-  text-decoration: none;
-  &:hover {
-    border-bottom: 1px dotted white;
-    padding-bottom: 2px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
