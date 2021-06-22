10287c01e26fde02d40c40ef423860f2db4e43b5
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-06-22 16:09:10 +0100

    production setup

--- backend/adonis5-server.config.js
+++ backend/adonis5-server.config.js
@@ -1,18 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: 'Gotedo_Adonis_4.1_Api_Server',
-      script: './server.js',
+      name: 'Adonis5_Demo_Api_Server',
+      script: './build/server.js',
       watch: true,
       ignore_watch: ['public', 'private_uploads'],
       watch_options: {
         persistent: true,
         ignoreInitial: true,
       },
-      env: {
+      env_production: {
         NODE_ENV: 'production',
       },
+      env_demo: {
+        NODE_ENV: 'demo',
+      },
       instances: 1,
+      autorestart: true,
+      exec_mode: 'cluster',
     },
   ],
-};
+}
