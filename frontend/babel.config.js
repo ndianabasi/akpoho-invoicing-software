64623ae869cdd8f27013a6cd79909f319eb3b2ef
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-06-22 19:39:02 +0100

    fix: attempt to fix frontend build error

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,4 @@
 /* eslint-env node */
 module.exports = {
   presets: ['@quasar/babel-preset-app'],
-  plugins: [
-    [
-      'babel-plugin-dotenv',
-      {
-        replacedModuleName: 'babel-dotenv',
-      },
-    ],
-  ],
 };
