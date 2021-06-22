51468d80be37c76d942031567dfdcd59b42f4143
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-06-22 14:50:38 +0100

    fix: improve proxy connection settings for production

--- backend/config/app.ts
+++ backend/config/app.ts
@@ -77,7 +77,7 @@ export const http: ServerConfig = {
   | headers.
   |
   */
-  trustProxy: proxyAddr.compile('loopback'),
+  trustProxy: proxyAddr.compile(true),
 
   /*
   |--------------------------------------------------------------------------
