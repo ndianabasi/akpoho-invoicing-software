7f17056cc8f9a546bb4a7666f16b0dbb0b262f24
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-07-23 03:07:43 +0100

    fix: resolve null issue in `useThousandSeparator` hook

--- frontend/src/composables/useThousandSeparators.ts
+++ frontend/src/composables/useThousandSeparators.ts
@@ -1,6 +1,7 @@
 export default function (input: number, decimalPlaces?: number) {
   const minimumFractionDigits = decimalPlaces ?? 2;
-  return input.toLocaleString(undefined, {
+  const inputNumber = input ?? 0;
+  return inputNumber.toLocaleString(undefined, {
     minimumFractionDigits,
     useGrouping: true,
   });
