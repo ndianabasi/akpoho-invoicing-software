810b45aa4b2a4ffb0e8a4574271979e7ff3aa5e8
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-07-25 20:42:19 +0100

    fix: remove missing components

--- print-backend/src/pages/Index.vue
+++ print-backend/src/pages/Index.vue
@@ -10,40 +10,13 @@
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
+  components: {},
   setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+    return {};
+  },
 });
 </script>
