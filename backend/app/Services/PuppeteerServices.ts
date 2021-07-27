8256010d7a3a7b34b598c1320f2ef8564ae0ee94
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-07-27 13:42:32 +0100

    fix: remove catch block in `printAsPDF` method

--- backend/app/Services/PuppeteerServices.ts
+++ backend/app/Services/PuppeteerServices.ts
@@ -24,14 +24,13 @@ export default class PuppeteerServices {
   public async printAsPDF() {
     const ctx = HttpContext.get()
 
-    try {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-      // 1. Create PDF from URL
-      await page.goto(this.url)
+    // 1. Create PDF from URL
+    await page.goto(this.url)
 
-      /* await page
+    /* await page
         .waitForNavigation({
           timeout: 1 * 60 * 1000, // 2 minute timeout
           waitUntil: 'networkidle0', //consider navigation to be finished when there are no more than 0 network connections for at least 500 ms
@@ -40,30 +39,26 @@ export default class PuppeteerServices {
           console.log('waitForNavigation done')
         }) */
 
-      // 2. Save a PDF
-      await this.prepareFilePath(this.fileName).then(async (filePath) => {
-        await page.emulateMediaType('screen')
-        await page
-          .pdf({
-            path: filePath,
-            format: this.paperFormat,
-            scale: 0.8,
-            printBackground: true,
-            margin: { left: '20px', right: '20px', top: '20px', bottom: '20px' },
-          })
-          .then(() => console.log('File created'))
-          .catch((error) => {
-            Logger.error('Error at PuppeteerServices.printAsPDF > page.pdf(): %j', error)
-            ctx?.response.internalServerError({ data: error })
-          })
+    // 2. Save a PDF
+    await this.prepareFilePath(this.fileName).then(async (filePath) => {
+      await page.emulateMediaType('screen')
+      await page
+        .pdf({
+          path: filePath,
+          format: this.paperFormat,
+          scale: 0.8,
+          printBackground: true,
+          margin: { left: '20px', right: '20px', top: '20px', bottom: '20px' },
+        })
+        .then(() => console.log('File created'))
+        .catch((error) => {
+          Logger.error('Error at PuppeteerServices.printAsPDF > page.pdf(): %j', error)
+          ctx?.response.internalServerError({ data: error })
+        })
 
-        await browser.close()
-        ctx?.response.download(filePath)
-      })
-    } catch (error) {
-      Logger.error('Error at PuppeteerServices.printAsPDF > catch block: %j', error)
-      ctx?.response.internalServerError({ message: error })
-    }
+      await browser.close()
+      ctx?.response.download(filePath)
+    })
   }
 
   /**
